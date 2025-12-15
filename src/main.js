3cfa0dd125baba160cc8a223720605e472aeb693
nbintang <nbintangh@gmail.com>
2025-12-15 15:08:55 +0700

    kntl

--- src/main.js
+++ src/main.js
@@ -197,7 +197,7 @@ async function subscribePushNotification(registration) {
 
 // Register Service Worker
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/public/sw.js")
+  navigator.serviceWorker.register("/sw.js")
     .then(reg => {
       console.log("[App] Service Worker registered:", reg.scope);
       
